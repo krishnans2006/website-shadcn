acf6f8e223f55f374074f7dd7aa54d28696e7a76
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-04 19:22:52 -0400

    Add social icons to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Users } from 'lucide-svelte';
+	import { Github, Linkedin, Users } from 'lucide-svelte';
 
 	import * as Avatar from '$lib/components/ui/avatar';
 	import { Button } from '$lib/components/ui/button/index.js';
@@ -16,14 +16,14 @@
 	<div class="flex-auto flex flex-col justify-center gap-4">
 		<h1 class="text-4xl font-bold">Hey, I'm Krishnan 🌊</h1>
 		<h3 class="text-lg">I'm a rising freshman at the University of Illinois, Urbana-Champaign.</h3>
-		<div class="flex gap-2">
+		<div class="flex gap-2 items-center">
 			<DropdownMenu.Root>
 				<DropdownMenu.Trigger asChild let:builder>
 					<Button builders={[builder]} class="w-28">Projects</Button>
 				</DropdownMenu.Trigger>
 				<DropdownMenu.Content class="w-44">
 					<DropdownMenu.Item href="/hackathons" class="cursor-pointer">
-						<Users class="mr-2 size-4"/>
+						<Users class="mr-2 size-4" />
 						<span>Hackathons</span>
 					</DropdownMenu.Item>
 				</DropdownMenu.Content>
@@ -37,6 +37,14 @@
 			>
 				Resume
 			</Button>
+			<div class="ml-2">
+				<Button variant="link" size="icon" href="https://github.com/krishnans2006" target="_blank" rel="noopener noreferrer">
+					<Github class="size-7" />
+				</Button>
+				<Button variant="link" size="icon" href="https://linkedin.com/in/krishnan-shankar" target="_blank" rel="noopener noreferrer">
+					<Linkedin class="size-7" />
+				</Button>
+			</div>
 		</div>
 	</div>
 </div>
