2efb81c1f4ba4aa8cceb84c40d3270a04a954f34
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-04 18:08:00 -0400

    Implement home page layout

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -83,6 +83,7 @@
 			<span class="sr-only">Toggle theme</span>
 		</Button>
 
+		<!-- The below is preserved as a dropdown menu example -->
 		<!--		<DropdownMenu.Root>-->
 		<!--			<DropdownMenu.Trigger asChild let:builder>-->
 		<!--				<Button-->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import * as Avatar from "$lib/components/ui/avatar";
+</script>
+
+<div class="flex gap-8 md:gap-12 mt-24 flex-col items-center md:flex-row">
+	<Avatar.Root class="flex-none size-72 md:size-96">
+		<Avatar.Image src="https://media.licdn.com/dms/image/D4D03AQHGfYRRgHKjhA/profile-displayphoto-shrink_800_800/0/1631249189059?e=1725494400&v=beta&t=t1JMEEth8crgr993ylQA-j7mAEMHBdPrTfWqTa5loM4" alt="@krishnans2006" />
+		<Avatar.Fallback>KS</Avatar.Fallback>
+	</Avatar.Root>
+	<div class="flex-auto flex flex-col justify-center gap-4">
+		<h1 class="text-4xl font-bold">Hey, I'm Krishnan 🌊</h1>
+		<h3 class="text-lg">I'm a rising freshman at the University of Illinois, Urbana-Champaign.</h3>
+	</div>
+</div>
