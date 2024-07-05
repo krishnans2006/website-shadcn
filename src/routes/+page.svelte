1c8e115b89a10141529c7169707b2e6296b4318b
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-04 20:18:13 -0400

    Update home page image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 <div class="flex gap-8 md:gap-12 mt-24 flex-col items-center md:flex-row">
 	<Avatar.Root class="flex-none size-72 md:size-96">
 		<Avatar.Image
-			src="https://media.licdn.com/dms/image/D4D03AQHGfYRRgHKjhA/profile-displayphoto-shrink_800_800/0/1631249189059?e=1725494400&v=beta&t=t1JMEEth8crgr993ylQA-j7mAEMHBdPrTfWqTa5loM4"
+			src="./picture.png"
 			alt="@krishnans2006" />
 		<Avatar.Fallback>KS</Avatar.Fallback>
 	</Avatar.Root>
