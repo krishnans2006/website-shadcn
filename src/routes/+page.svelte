c0e93618682b7a703098eb89bcb9cc427d50440e
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-07 00:24:00 -0400

    Add hover cards to badges for Turn-In, format HTML

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,12 +18,12 @@
 	} from 'lucide-svelte';
 
 	import * as Avatar from '$lib/components/ui/avatar';
-  import { Badge } from "$lib/components/ui/badge";
+	import { Badge } from '$lib/components/ui/badge';
 	import { Button } from '$lib/components/ui/button/index.js';
-  import * as Card from "$lib/components/ui/card";
-	import * as Carousel from "$lib/components/ui/carousel/index.js";
+	import * as Card from '$lib/components/ui/card';
+	import * as Carousel from '$lib/components/ui/carousel/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-  import * as HoverCard from "$lib/components/ui/hover-card";
+	import * as HoverCard from '$lib/components/ui/hover-card';
 </script>
 
 <svelte:head>
@@ -31,7 +31,8 @@
 </svelte:head>
 
 <div class="flex flex-col xl:min-h-dvh grow flex-none items-center">
-	<div class="flex gap-10 xl:gap-12 mt-6 md:mt-12 lg:mt-24 flex-col items-center xl:flex-row xl:flex-auto">
+	<div
+		class="flex gap-10 xl:gap-12 mt-6 md:mt-12 lg:mt-24 flex-col items-center xl:flex-row xl:flex-auto">
 		<div class="flex-none basis-1/3 flex justify-center mb-8 xl:mb-0">
 			<Avatar.Root class="size-72 md:size-96">
 				<Avatar.Image
@@ -191,7 +192,8 @@
 				<Card.Root class="p-3">
 					<Card.Content class="flex flex-row gap-4 items-center p-6">
 						<div class="w-1/3">
-							<img src="https://tin.tjhsst.edu/static/favicon/android-chrome-maskable-512x512.png" alt="Tin Logo" class="object-fill size-full rounded-3xl" />
+							<img src="https://tin.tjhsst.edu/static/favicon/android-chrome-maskable-512x512.png"
+									 alt="Tin Logo" class="object-fill size-full rounded-3xl" />
 						</div>
 						<div class="w-2/3">
 							<h1 class="text-5xl font-bold">Turn-In</h1>
@@ -218,23 +220,68 @@
 									<Badge class="bg-[#EF662A] hover:bg-[#E34E26] text-white">HTML/CSS/JS</Badge>
 								</HoverCard.Trigger>
 								<HoverCard.Content class="w-80">
-									<a class="flex flex-row justify-between space-x-4" href="https://react.dev" target="_blank" rel="noopener noreferrer">
+									<a class="flex flex-row justify-between space-x-4" href="https://en.wikipedia.org/wiki/HTML"
+										 target="_blank" rel="noopener noreferrer">
 										<Avatar.Root>
-											<Avatar.Image src="https://www.svgrepo.com/show/303500/react-1-logo.svg" alt="React Logo" />
+											<Avatar.Image src="https://icons.veryicon.com/png/System/Plex/Other%20html%205.png"
+																		alt="HTML Logo" />
 											<Avatar.Fallback>R</Avatar.Fallback>
 										</Avatar.Root>
 										<div class="space-y-1">
 											<div class="flex">
-												<h4 class="text-sm font-semibold">React</h4>
+												<h4 class="text-sm font-semibold">HTML, CSS, and JavaScript</h4>
 												<ExternalLink class="size-4 ml-2" />
 											</div>
-											<p class="text-sm">The library for web and native user interfaces</p>
+											<p class="text-sm">The standard markup, styling, and scripting languages for
+												web development</p>
 										</div>
 									</a>
 								</HoverCard.Content>
 							</HoverCard.Root>
-							<Badge class="bg-[#0C4C32] hover:bg-[#103E2E] text-white">Django</Badge>
-							<Badge class="bg-[#326690] hover:bg-[#2c5b81] text-white">PostgreSQL</Badge>
+							<HoverCard.Root>
+								<HoverCard.Trigger>
+									<Badge class="bg-[#0C4C32] hover:bg-[#103E2E] text-white">Django</Badge>
+								</HoverCard.Trigger>
+								<HoverCard.Content class="w-80">
+									<a class="flex flex-row justify-between space-x-4" href="https://www.djangoproject.com"
+										 target="_blank" rel="noopener noreferrer">
+										<Avatar.Root>
+											<Avatar.Image src="https://www.svgrepo.com/show/353657/django-icon.svg"
+																		alt="Django Logo" />
+											<Avatar.Fallback>R</Avatar.Fallback>
+										</Avatar.Root>
+										<div class="space-y-1">
+											<div class="flex">
+												<h4 class="text-sm font-semibold">Django</h4>
+												<ExternalLink class="size-4 ml-2" />
+											</div>
+											<p class="text-sm">Django makes it easier to build better web apps more quickly and with less code</p>
+										</div>
+									</a>
+								</HoverCard.Content>
+							</HoverCard.Root>
+							<HoverCard.Root>
+								<HoverCard.Trigger>
+									<Badge class="bg-[#326690] hover:bg-[#2c5b81] text-white">PostgreSQL</Badge>
+								</HoverCard.Trigger>
+								<HoverCard.Content class="w-80">
+									<a class="flex flex-row justify-between space-x-4" href="https://www.postgresql.org"
+										 target="_blank" rel="noopener noreferrer">
+										<Avatar.Root>
+											<Avatar.Image src="https://i.pinimg.com/originals/06/86/c0/0686c0c85407548ea5bd737a572974b6.png"
+																		alt="PostgreSQL Logo" />
+											<Avatar.Fallback>R</Avatar.Fallback>
+										</Avatar.Root>
+										<div class="space-y-1">
+											<div class="flex">
+												<h4 class="text-sm font-semibold">PostgreSQL</h4>
+												<ExternalLink class="size-4 ml-2" />
+											</div>
+											<p class="text-sm">The world's most advanced open source relational database</p>
+										</div>
+									</a>
+								</HoverCard.Content>
+							</HoverCard.Root>
 						</div>
 					</Card.Content>
 				</Card.Root>
@@ -245,11 +292,13 @@
 				<Card.Root class="p-3">
 					<Card.Content class="flex flex-row gap-4 items-center p-6">
 						<div class="w-1/3">
-							<img src="https://avatars.githubusercontent.com/u/47068591?s=1200" alt="TJUAV Logo" class="object-fill size-full rounded-3xl" />
+							<img src="https://avatars.githubusercontent.com/u/47068591?s=1200" alt="TJUAV Logo"
+									 class="object-fill size-full rounded-3xl" />
 						</div>
 						<div class="w-2/3">
 							<h1 class="text-5xl font-bold">TJUAV GroundStation</h1>
-							<h3 class="text-2xl">An Open-Source, Performance-Driven Implementation of Ardupilot</h3>
+							<h3 class="text-2xl">An Open-Source, Performance-Driven Implementation of
+								Ardupilot</h3>
 							<br />
 							<p>
 								The go-to Ardupilot Ground Control Station (GCS) is Mission Planner, an application
@@ -279,11 +328,13 @@
 				<Card.Root class="p-3">
 					<Card.Content class="flex flex-row gap-4 items-center p-6">
 						<div class="w-1/3">
-							<img src="https://avatars.githubusercontent.com/u/12674382?s=1200" alt="HackTJ Logo" class="object-fill size-full rounded-3xl" />
+							<img src="https://avatars.githubusercontent.com/u/12674382?s=1200" alt="HackTJ Logo"
+									 class="object-fill size-full rounded-3xl" />
 						</div>
 						<div class="w-2/3">
 							<h1 class="text-5xl font-bold">HackTJ Portal</h1>
-							<h3 class="text-2xl">A Privacy-Focused, FERPA-Compliant Hackathon Submission and Judging Platform</h3>
+							<h3 class="text-2xl">A Privacy-Focused, FERPA-Compliant Hackathon Submission and
+								Judging Platform</h3>
 							<br />
 							<p>
 								Previously, HackTJ, an annual student-run high school hackathon, used Devpost to
