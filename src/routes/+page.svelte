c88330ba8218b32865374632d6c99cfb6e43cc39
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-06 21:45:39 -0400

    Improve Turn-In carousel description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -201,6 +201,14 @@
 								per period, per assignment. As you can imagine, this is both time-consuming and
 								risky from a security standpoint (what if a student submits malicious code?).
 							</p>
+							<br />
+							<p>
+								Turn-In solves this problem by enabling functionality for code autograders. All a
+								teacher needs to do is write the grader script for an assignment, and students will
+								be able to submit their code to Turn-In for grading. Turn-In will run the grader
+								and give students instant feedback, allowing students to learn from their mistakes
+								while saving teachers time and effort.
+							</p>
 						</div>
 					</Card.Content>
 				</Card.Root>
