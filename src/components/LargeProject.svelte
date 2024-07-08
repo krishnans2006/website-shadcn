e7d865ac06e94b9c8a9bf1b5561de619f7068249
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-07 22:21:54 -0400

    Add LargeProject component to abstract projects in carousel

---
+++ src/components/LargeProject.svelte
@@ -0,0 +1,14 @@
+<script>
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<Card.Root class="p-3">
+	<Card.Content class="flex flex-row gap-4 items-center p-6">
+		<div class="w-1/3">
+			<slot name="image"></slot>
+		</div>
+		<div class="w-2/3">
+			<slot name="text"></slot>
+		</div>
+	</Card.Content>
+</Card.Root>
