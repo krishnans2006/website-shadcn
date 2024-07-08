0d2437d80f59630c0184f4c7b8dabfd9817f4b06
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-07 22:34:51 -0400

    Add mobile styling to LargeProject.svelte

--- src/components/LargeProject.svelte
+++ src/components/LargeProject.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <Card.Root class="p-3">
-	<Card.Content class="flex flex-row gap-4 items-center p-6">
-		<div class="w-1/3">
+	<Card.Content class="flex flex-col md:flex-row gap-4 items-center p-1 md:p-6">
+		<div class="md:w-1/3">
 			<slot name="image"></slot>
 		</div>
-		<div class="w-2/3">
+		<div class="md:w-2/3">
 			<slot name="text"></slot>
 		</div>
 	</Card.Content>
