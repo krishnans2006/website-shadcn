1b959d17827912a4800af4ee7625bc750cafda34
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-12 00:59:09 -0400

    Add high school page

---
+++ src/routes/high-school/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Krishnan Shankar - High School</title>
+</svelte:head>
+
+<div class="my-6">
+	<h1 class="text-4xl font-bold text-center">My High School Experience</h1>
+	<h4 class="text-lg text-center">I attended Thomas Jefferson High School for Science and Technology.</h4>
+</div>
