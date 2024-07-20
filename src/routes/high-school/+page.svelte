f43b050e65e2c63387fc4e7e7f38618784b459d2
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-20 19:29:57 -0400

    Move Lead Sysadmin SidePopup to component

--- src/routes/high-school/+page.svelte
+++ src/routes/high-school/+page.svelte
@@ -4,7 +4,7 @@
 	import { Separator } from '$lib/components/ui/separator';
 
 	import { ArrowDownToLine } from 'lucide-svelte';
-	import SidePopup from '../../components/SidePopup.svelte';
+	import LeadSysadmin from '../../components/sidepopups/LeadSysadmin.svelte';
 </script>
 
 <svelte:head>
@@ -116,73 +116,5 @@
 </div>
 
 <div class="flex flex-col md:flex-row">
-	<SidePopup project={{ title: "Lead Sysadmin", description: "TJ Computer Systems Lab" }}>
-		<svelte:fragment slot="small">
-			<p>Led a team of 15 students building apps that power the school community.</p>
-		</svelte:fragment>
-		<svelte:fragment slot="large">
-			<div class="flex flex-col lg:flex-row gap-4">
-				<div class="animate-fade-up delay-1000 lg:basis-1/2">
-					<Card.Root class="transition-transform delay-75 duration-300 ease-in-out">
-						<Card.Header class="flex flex-row">
-							<img src="$lib/images/projects/turnin.png" alt="Turn-In Logo"
-									 class="object-fill size-12 rounded-xl mr-3" />
-							<div class="w-4/5">
-								<Card.Title>Developed Turn-In</Card.Title>
-								<Card.Description>A fast and secure code autograder</Card.Description>
-							</div>
-						</Card.Header>
-						<Card.Content>
-							<p>
-								Turn-In is a fast and secure code autograder for TJ's computer science classes.
-								Previously, teachers had to manually run and grade student code, which was both
-								time-consuming and unsafe (the easiest form of remote code execution!).
-							</p>
-							<br />
-							<p>
-								Turn-In solves this problem by using teacher-provided grader scripts to
-								automatically grade student code, providing students with instant feedback for
-								improvement while letting teachers focus on teaching.
-							</p>
-							<br />
-							<p>
-								Currently, Turn-In is used by every CS class at TJ (classes like APCS, AI, and ML),
-								with over <strong>200,000 graded student submissions</strong> in just the 2023-2024
-								school year.
-							</p>
-						</Card.Content>
-					</Card.Root>
-				</div>
-				<div class="animate-fade-up delay-1000 lg:basis-1/2">
-					<Card.Root class="transition-transform delay-75 duration-300 ease-in-out">
-						<Card.Header class="flex flex-row">
-							<img src="$lib/images/badges/ceph.png" alt="Ceph Logo"
-									 class="object-fill size-12 rounded-xl mr-3" />
-							<div class="w-4/5">
-								<Card.Title>Configured Ceph</Card.Title>
-								<Card.Description>A high-availability, triple-replicated storage cluster</Card.Description>
-							</div>
-						</Card.Header>
-						<Card.Content>
-							<p>
-								As a vital part of the TJ community, the TJ Computer Systems Lab (TJ CSL) needs to
-								store a lot of data reliably. For example, we store PII (personal identifiable
-								information) for students, <strong>legal records like attendance reports and
-								student grades</strong> (see Turn-In), and much more. This requires a secure and
-								reliable storage solution like Ceph, the industry standard.
-							</p>
-							<br />
-							<p>
-								After my work with Ceph, it now <strong>stores over 55 TB of data</strong>
-								ranging from VM disks to S3-compatible logs to a network filesystem. Besides
-								powering crucial TJ CSL applications, Ceph's filesystem is used by hundreds of
-								students and teachers to conduct research, build large ML models, and even work on
-								personal projects using the lab's resources.
-							</p>
-						</Card.Content>
-					</Card.Root>
-				</div>
-			</div>
-		</svelte:fragment>
-	</SidePopup>
+	<LeadSysadmin />
 </div>
