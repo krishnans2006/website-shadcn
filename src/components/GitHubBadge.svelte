ff18311d9a1e29b543c62f35ad401bcbb809ff3d
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-22 19:10:24 -0400

    Add preliminary GitHubBadge component

---
+++ src/components/GitHubBadge.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+
+	export let title: string = '';
+	export let description: string = '';
+	export let url: string = '';
+</script>
+
+<a href={url}>
+	<Card.Root>
+		<Card.Header>
+			<Card.Title>{title}</Card.Title>
+			<Card.Description>{description}</Card.Description>
+		</Card.Header>
+	</Card.Root>
+</a>
