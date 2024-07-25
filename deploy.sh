1c73df83e79fa74425338218c76e00935704be98
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-25 03:15:23 -0400

    Make deploy script re-usable

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 pnpm run build
 
-gh-pages -r git@github.com:krishnans2006/krishnans2006.github.io.git -b main -d build --nojekyll -m "Deploy website redesign"
+timestamp=$(pnpm node -e "console.log(new Date())")
+gh_pages_args="-b main -d build --nojekyll -m \"Update $timestamp\""
+
+gh-pages -r git@github.com:krishnans2006/krishnans2006.github.io.git "$gh_pages_args"
