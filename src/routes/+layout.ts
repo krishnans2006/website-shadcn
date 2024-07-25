d5e3bc22ec88d4cfe19e66892aa346f62fa4ef08
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-25 02:30:32 -0400

    Implement SvelteKit prerendering

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import routeMap from '../data/routes.json'
 import type { LayoutLoad } from './$types';
 
+export const prerender = true;
+
 export const load: LayoutLoad = async () => {
 	return {
 		routes: routeMap.routes,
