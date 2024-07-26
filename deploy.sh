2f3d89c2c32395808a1a02ac8ec76906e76f7bc8
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-25 21:19:42 -0400

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 pnpm run build
 
 timestamp=$(pnpm node -e "console.log(new Date())")
-gh_pages_args="-b main -d build --nojekyll -m \"Update $timestamp\""
 
-gh-pages -r git@github.com:krishnans2006/krishnans2006.github.io.git "$gh_pages_args"
+gh-pages -r git@github.com:krishnans2006/krishnans2006.github.io.git -b main -d build --nojekyll -m "Update $timestamp"
